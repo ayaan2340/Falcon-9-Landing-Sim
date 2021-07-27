6a45009c4cd7d1e41f6b5f3205a11060a0645535
Ayaan <ayaansunesara123@gmail.com>
2021-07-26 21:49:30 -0500

    Update

--- homepage.js
+++ homepage.js
@@ -3,15 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
     document.addEventListener('mousemove', (event) =>
     {
         getXYPos(event);
-        document.documentElement.style.setProperty('--xpos', XPercent);
-        document.documentElement.style.setProperty('--ypos', YPercent);
         document.body.style.backgroundPosition = XPercent.toString() + "% " + YPercent.toString() + "%";
-    }
-    );
-    
+    });
+    document.addEventListener('touchmove', () => {
+        document.body.style.animation = 'panning 25s infinite ease-out';
+    });
 });
 
 function getXYPos (event) {
-    XPercent = (event.screenX / screen.width * 50) + 24;
-    YPercent = (event.screenY/ screen.height * 50) + 18;
+    XPercent = ((event.screenX - window.screen.availLeft) / window.screen.width * 50) + 24;
+    YPercent = ((event.screenY - window.screen.availTop) / window.screen.height * 50) + 18;
 }
