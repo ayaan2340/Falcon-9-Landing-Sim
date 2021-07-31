b2cbfbdd9b34ae6d90fd6cf7c5c893aa7644e775
Ayaan <ayaansunesara123@gmail.com>
2021-07-31 15:11:26 -0500

    Update RocketMovement.cs

--- New Unity Project/Assets/RocketMovement.cs
+++ New Unity Project/Assets/RocketMovement.cs
@@ -2,23 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+public class RocketMovement : MonoBehaviour
+{ 
     // Update is called once per frame
     void Update()
     {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        Vector3 vertical = new Vector3(-0.4f*(rb.rotation), 0f, 0f);
         if (Input.GetKey(KeyCode.W))
-            transform.position += transform.up * Time.deltaTime * 10f;
+            rb.AddForce(vertical);
         if (Input.GetKey(KeyCode.A)) 
-            rb.MoveRotation(rb.rotation + 0.25f * Time.deltaTime);
+            rb.MoveRotation(rb.rotation + 0.5f);
         if (Input.GetKey(KeyCode.D))  
-            rb.MoveRotation(rb.rotation + -0.25f * Time.deltaTime);
-    }       
+            rb.MoveRotation(rb.rotation + -0.5f);
+        if(Input.GetKey(KeyCode.S))
+            rb.AddForce(-vertical);
+    } 
 }
